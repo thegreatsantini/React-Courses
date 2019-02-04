3690069a05996dfb895b69094c3193feb8ad26c4
thegreatsantini <lss13@my.fsu.edu>
2019-02-04 13:53:15 -0800

    finished vid 5

---
+++ Advanced-React/sick-fits/frontend/components/Header.js
@@ -0,0 +1,15 @@
+import Nav from './Nav'
+const Header = () => (
+  <div>
+    <div class="bar">
+      <a href="">Sick Fits</a>
+      <Nav />
+    </div>
+    <div class="sub-bar">
+      <p>Search</p>
+    </div>
+    <div>Cart</div>
+  </div>
+);
+
+export default Header;
---
+++ Advanced-React/sick-fits/frontend/components/Page.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Meta from './Meta'
+import Header from './Header'
+class Page extends Component {
+    render() {
+        return (
+            <div>
+                <Meta />
+                <Header />
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+export default Page;
